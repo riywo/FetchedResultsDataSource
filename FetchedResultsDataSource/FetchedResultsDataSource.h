a4fc8109543d6bc7f956aef01892ef8c0bfb4bc5
Ryosuke IWANAGA <me@riywo.com>
2018-09-03 03:29:59 -0700

    Initial Commit

---
+++ FetchedResultsDataSource/FetchedResultsDataSource.h
@@ -0,0 +1,19 @@
+//
+//  FetchedResultsDataSource.h
+//  FetchedResultsDataSource
+//
+//  Created by Ryosuke Iwanaga on 2018-09-03.
+//  Copyright © 2018 Ryosuke Iwanaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FetchedResultsDataSource.
+FOUNDATION_EXPORT double FetchedResultsDataSourceVersionNumber;
+
+//! Project version string for FetchedResultsDataSource.
+FOUNDATION_EXPORT const unsigned char FetchedResultsDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FetchedResultsDataSource/PublicHeader.h>
+
+
